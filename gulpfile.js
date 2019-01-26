852f83edb8508a2f87cb3ca116c6f9ba68f5a0fc
Anton Romankov <romant094@gmail.com>
2019-01-26 12:40:22 +0300

    Added 5 of 7 sections

--- gulpfile.js
+++ gulpfile.js
@@ -33,52 +33,15 @@ gulp.task('img', () => {
     return stream;
 });
 
-gulp.task('browser-sync', function () {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch('./src/index.html', browserSync.reload);
+    gulp.watch('./src/scss/style.css', browserSync.reload);
 });
 
-
-
-// gulp.task('sass', function () {
-//     var stream = gulp.src([config.sassPath + '/style.scss'])
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(autoprefixer({
-//             browsers: ['last 2 versions'],
-//             cascade: false
-//         }))
-//         .pipe(minifycss({ compatibility: 'ie8' }))
-//         .pipe(rename('style.min.css'))
-//         .pipe(gulp.dest(config.destPath + '/css'));
-//     return stream;
-// });
-
-// gulp.task('js', function() {
-//     var stream = gulp.src([config.bowerDir + '/jquery/dist/jquery.js', config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.js', config.jsPath + '/*.js'])
-//         .pipe(concat('script.js'))
-//         .pipe(uglify())
-//         .pipe(rename('script.min.js'))
-//         .pipe(gulp.dest(config.destPath)); 
-//     return stream;
-// });
-
-// gulp.task('doStuff', function (done) {
-//     done();
-// });
-// gulp.task('watch', function () {
-//     gulp.watch(['dist/**'], 'doStuff')
-//         .on('change', function (file) {
-//             fileChanged(file.path);
-//         });
-// });
-
-// gulp.task('watch', function () {
-//     watch([config.sassPath + '/*.scss'], (event, cb) => {
-//         gulp.start('sass');
-//     });
-// watch([config.jsPath + '/*.js'], function (event, cb) {
-//     gulp.start('js');
-// });
-// });
-
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src/"
+        }
+    });
+});
